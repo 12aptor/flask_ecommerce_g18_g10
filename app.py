c7598712e8d80346ef753c93f446225d8133fc94
Paolo Soncco <paholo2054@gmail.com>
2023-01-21 10:41:52 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+@app.route("/")
+def index():
+    return "<button>Dame click</button>"
+
+import routers.productos_router
